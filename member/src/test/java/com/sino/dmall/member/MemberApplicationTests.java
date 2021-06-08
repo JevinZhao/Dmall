256d835e839e459a1820003085446ea40a9356f8
Jevin Zhao <JevinZhao@gmail.com>
2021-06-08 22:09:34 +0800

    Project Init

---
+++ member/src/test/java/com/sino/dmall/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sino.dmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ware/src/test/java/com/sino/dmall/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sino.dmall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
